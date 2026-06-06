c1054c55c3ee8e27ccbfa8e888810ca6f844dc10
hansie1111 <hansiepansie007@gmail.com>
2026-06-06 11:48:33 +0200

    Create onfig.py

---
+++ onfig.py
@@ -0,0 +1,63 @@
+# ========================================
+# CRYPTO TRADING ASSISTANT - CONFIGURATIE
+# (DUMMY DATA - voor GitHub)
+# ========================================
+
+PORTFOLIO = {
+    "render-token": {
+        "amount": 0,
+        "percentage": 21.89,
+        "name": "RENDER"
+    },
+    "avalanche-2": {
+        "amount": 0,
+        "percentage": 0,
+        "name": "AVAX"
+    },
+    "wormhole": {
+        "amount": 0,
+        "percentage": 19.41,
+        "name": "W"
+    },
+    "peaq": {
+        "amount": 0,
+        "percentage": 10.65,
+        "name": "PEAQ",
+        "manual_price": 0.02252
+    },
+    "renzo": {
+        "amount": 0,
+        "percentage": 9.40,
+        "name": "REZ"
+    },
+    "injective-protocol": {
+        "amount": 0,
+        "percentage": 9.11,
+        "name": "INJ"
+    },
+    "centrifuge": {
+        "amount": 0,
+        "percentage": 9.09,
+        "name": "CFG"
+    },
+    "fetch-ai": {
+        "amount": 0,
+        "percentage": 7.80,
+        "name": "FET"
+    },
+    "dash": {
+        "amount": 0,
+        "percentage": 4.63,
+        "name": "DASH"
+    },
+    "filecoin": {
+        "amount": 0,
+        "percentage": 0,
+        "name": "FIL"
+    },
+    "ethereum-name-service": {
+        "amount": 0,
+        "percentage": 0,
+        "name": "ENS"
+    },
+}
